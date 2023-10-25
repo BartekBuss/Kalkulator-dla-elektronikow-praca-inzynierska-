2aafc9e7e8bb0898e32b2844a7c749dde9b6b082
BartekBuss <129660846+BartekBuss@users.noreply.github.com>
2023-10-25 10:54:49 +0200

    Add files via upload

--- components/KatFazowyZLiczbyZespolonej.js
+++ components/KatFazowyZLiczbyZespolonej.js
@@ -1,15 +1,79 @@
-import React from 'react';
-import { View, Text } from 'react-native';
-import { commonStyles as styles } from './styles/styles';
+import React, { useState } from 'react';
+import { View, Text, TextInput, Button, StyleSheet } from 'react-native';
 
-class KatFazowyZLiczbyZespolonej extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Ekran Kąt Fazowy z Liczby Zespolonej</Text>
+const KatFazowyZLiczbyZespolonej = () => {
+  const [value1, setValue1] = useState('');
+  const [value2, setValue2] = useState('');
+  const [result, setResult] = useState(0);
+  const [result2, setResult2] = useState(0);
+
+  const calculateResult = () => {
+    const parsedValue1 = parseFloat(value1);
+    const parsedValue2 = parseFloat(value2);
+
+    if (parsedValue1 === 0) {
+      setResult("Nie można dzielić przez 0");
+      setResult2("Nie można dzielić przez 0");
+    } else {
+      const calculatedResult = Math.atan(parsedValue2 / parsedValue1);
+      const calculatedResult2 = parsedValue1 * (180/Math.PI);
+      setResult(calculatedResult);
+      setResult2(calculatedResult2);
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.input}
+          placeholder="Re"
+          keyboardType="numeric"
+          value={value1}
+          onChangeText={(text) => setValue1(text)}
+        />
       </View>
-    );
-  }
-}
+
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.input}
+          placeholder="Im"
+          keyboardType="numeric"
+          value={value2}
+          onChangeText={(text) => setValue2(text)}
+        />
+      </View>
+
+      <Button title="Oblicz" onPress={calculateResult} />
+
+      <View style={styles.resultContainer}>
+        <Text>Kąt fazowy [rad]: {result}</Text>
+        <Text>Kąt fazowy [stopnie]: {result2}</Text>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+  },
+  inputContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 10,
+  },
+  input: {
+    flex: 1,
+    marginRight: 10,
+    padding: 10,
+    borderWidth: 1,
+    borderRadius: 5,
+  },
+  resultContainer: {
+    marginTop: 20,
+  },
+});
 
 export default KatFazowyZLiczbyZespolonej;
